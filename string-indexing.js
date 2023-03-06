d5f54ad4c833d0b9077fcb38ef8f03d060a97392
Pratikd481 <pratikd481@gmail.com>
2023-03-06 22:32:59 +0530

    string indexing

---
+++ string-indexing.js
@@ -0,0 +1,7 @@
+let username = "Pratik das";
+//We can use string as array index like below example
+console.log(username[0]);
+console.log(username[2]);
+
+//Lenght of a stirng
+console.log(username.length);
