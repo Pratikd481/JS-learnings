960b947f2b0528cb2ae1f2682d37280598109bb9
Pratikd481 <pratikd481@gmail.com>
2023-03-06 22:28:23 +0530

    Var can be used before declaration because of hoisting

--- let-keyword.js
+++ let-keyword.js
@@ -1,3 +1,9 @@
-console.log(test);
-let test =  'test';
-console.log(test);
+//Var can be used before declaration because of hoisting 
+console.log(test); // prints undefined
+var test =  'test';
+console.log(test); //prints test
+
+//Can't use let variable before declaraion 
+// console.log(test); //let-keyword.js:7 Uncaught ReferenceError: Cannot access 'test' before initialization
+// let test =  'test';
+// console.log(test);
