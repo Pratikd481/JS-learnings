3aa9e5a16d92e39c22baf6d22f5816bcd7579b5d
Pratikd481 <pratikd481@gmail.com>
2023-03-06 22:52:17 +0530

    Truthy and falsy values in JS

---
+++ truthy-and-falsy.js
@@ -0,0 +1,13 @@
+//emppty stirng is falsy
+console.log(""?true:false);
+console.log("test"?true:false);
+
+//0 is falsy and except 0 everything is truthy
+console.log(0?true:false);
+console.log(1?true:false);
+
+//undefined is falsy 
+console.log(undefined?true:false);
+
+//null is falsy 
+console.log(null?true:false);
