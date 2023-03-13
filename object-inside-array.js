3dd5fdb58fbc0480ccbddb52443a1d5e3f2a3c53
Pratikd481 <pratikd481@gmail.com>
2023-03-13 20:07:10 +0530

    objects inside array

---
+++ object-inside-array.js
@@ -0,0 +1,9 @@
+const users = [
+    { userId: 1, firstName: "Pratik", lastName: "Das" },
+    { userId: 2, firstName: "Partha", lastName: "Das" },
+    { userId: 3, firstName: "Pankaj", lastName: "Sarkar" },
+];
+
+for (const user of users) {
+    console.log(user);
+}
