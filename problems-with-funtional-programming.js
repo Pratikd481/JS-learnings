b07e96d7b8a24d47d06507094c605242f8cf8b17
Pratikd481 <pratikd481@gmail.com>
2023-04-01 18:24:48 +0530

    Understanding __proto__ or [[prototype]]

--- problems-with-funtional-programming.js
+++ problems-with-funtional-programming.js
@@ -48,7 +48,7 @@ console.log(user3);
 console.log("============ Edning the ISSUE =====================");
 
 // So how we can overcome this issue ????
-console.log("============ Starting possible solution =====================");
+console.log("============ Starting solution  1 =====================");
 //Solution 1-------- Store methods in different object
 const userMethods = {
     about: function () {
@@ -72,6 +72,38 @@ const user4 = createUserSample1("user4", "Das", 24);
 console.log(user4.about());
 const user5 = createUserSample1("user5", "Sarkar", 24);
 console.log(user5.about());
-// Now if we console.log user2 and user3 we will found the issue
-console.log(user4);
-console.log(user5);
+//But this solution has another problem. If we want to add new methid in the user method object then to use that methid we have to add that in createUser funciton also. We might forget to add new method referenct in the createUser funciton
+console.log("============ Ending solution  1 =====================");
+
+
+
+
+
+
+// To solbe this issue we need to use __proto__ or [[prototype]]
+console.log("======== Understanding __proto__ or [[prototype]]===================");
+const obj1 = {
+    key1: "Value1",
+    key2: "Value 2",
+};
+console.log(obj1);
+console.log("Obj1 proto :", obj1.__proto__);
+
+const obj2 = Object.create(obj1);
+obj2.key3 = "Value3";
+//The abobe line will crate an empty object obj2 but it will set obj1 as a __proto__ /[[prototype]] of obj2.
+
+console.log(obj2);
+console.log("Obj2 proto :", obj2.__proto__);
+
+//Advantage of using __proto__
+// we can access keys of obj1 from obj2
+console.log("obj1 key accessed throught obj2: ",obj2.key1);
+console.log(obj2.key3);
+//Now if we want to access key3 through obj1. We can't becaue obj1 doesnot has obj2 as a __proto__
+console.log("obj2 key accessed throught obj1: ",obj1.key3);
+
+// How this works ??
+// If we are trying to access a key of a obect which is not available in the object the it will check for the key in its __proto__. If key present in its __proto__ then it will print that.
+
+console.log("======== END Understanding __proto__ or [[prototype]]===================");
