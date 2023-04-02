08665c626db0e303658a92495231fb2b5815062b
Pratikd481 <pratikd481@gmail.com>
2023-04-02 23:05:32 +0530

    Prototype property of a funciton

--- problems-with-funtional-programming.js
+++ problems-with-funtional-programming.js
@@ -75,11 +75,6 @@ console.log(user5.about());
 //But this solution has another problem. If we want to add new methid in the user method object then to use that methid we have to add that in createUser funciton also. We might forget to add new method referenct in the createUser funciton
 console.log("============ Ending solution  1 =====================");
 
-
-
-
-
-
 // To solbe this issue we need to use __proto__ or [[prototype]]
 console.log("======== Understanding __proto__ or [[prototype]]===================");
 const obj1 = {
@@ -98,17 +93,15 @@ console.log("Obj2 proto :", obj2.__proto__);
 
 //Advantage of using __proto__
 // we can access keys of obj1 from obj2
-console.log("obj1 key accessed throught obj2: ",obj2.key1);
+console.log("obj1 key accessed throught obj2: ", obj2.key1);
 console.log(obj2.key3);
 //Now if we want to access key3 through obj1. We can't becaue obj1 doesnot has obj2 as a __proto__
-console.log("obj2 key accessed throught obj1: ",obj1.key3);
+console.log("obj2 key accessed throught obj1: ", obj1.key3);
 
 // How this works ??
 // If we are trying to access a key of a obect which is not available in the object the it will check for the key in its __proto__. If key present in its __proto__ then it will print that.
 
 console.log("======== END Understanding __proto__ or [[prototype]]===================");
-
-
 
 console.log("*");
 console.log("*");
@@ -117,7 +110,7 @@ console.log("*");
 console.log("============ Starting solution  2 using __proto__ or [[prototype]]  =====================");
 
 function createUserSample3(firstName, lastName, age) {
-    const user =  Object.create(userMethods);
+    const user = Object.create(userMethods);
     user.firstName = firstName;
     user.lastName = lastName;
     user.age = age;
@@ -128,8 +121,39 @@ const user6 = createUserSample3("user6", "Das", 24);
 console.log(user6.about());
 const user7 = createUserSample3("user7", "Sarkar", 24);
 console.log(user7.about());
-console.log('user7 = ',user7);
+console.log("user7 = ", user7);
 
 /**
 In this way  we d not have to add new function (Which is added in userMethods) as a referance in the createUser fucntio. All newly added fucntion will be available in the __proto__ or [[prototype]. So if the called method is not available in createUser function it will check in the __proto__ or [[prototype].
  */
+console.log("============ Ending solution  2 using __proto__ or [[prototype]]  =====================");
+
+console.log("*");
+console.log("*");
+console.log("*");
+console.log("============ Understanding prototype property of function  =====================");
+/**
+We are solvig the issue for fuinctions by creating a new object which contains all the functions and then we assign that object  to the newly created object as __proto__. But without creating a new object for holding the function we can use prototype property of a fucntion .
+
+So what is prototype ??
+Only funciton provides prototype proerty. We can add anything to the prototype. In js function can also be treated as object. And it contains a prototype key by default. For example :-
+ */
+function hello() {
+    console.log("Hello world");
+}
+
+console.log(hello);
+console.log(hello.name);
+console.log("prototype of hello function : ", hello.prototype);
+
+hello.prototype.testKey = "testValue";
+console.log("prototype of hello function : ", hello.prototype);
+
+hello.prototype.sing = () => {
+    console.log("La la la");
+};
+console.log("prototype of hello function : ", hello.prototype);
+
+console.log("Calling sing function assigned in protorype :");
+hello.prototype.sing();
+console.log("============ End Understanding prototype property of function  =====================");
